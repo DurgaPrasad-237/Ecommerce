334aa28daa812ce6d4059fa53c85e947e09305c9
DurgaPrasad-237 <106289611+DurgaPrasad-237@users.noreply.github.com>
2023-08-04 13:01:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const x = document.getElementById('navbar');
+const bar = document.getElementById('bar');
+function opennav(){
+    x.style.right='0px';  
+}
+function closenav(){
+    x.style.right='-400px';
+}
